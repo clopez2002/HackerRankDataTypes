393474576a5f0a39028edecfc834b99587f3b502
lopezlin <morgan_pirata2003@yahoo.com.ar>
2021-06-22 21:42:16 -0300

    Resuleto

--- src/Solution.java
+++ src/Solution.java
@@ -3,7 +3,10 @@ import java.io.*;
 
 
 
+
 public class Solution{
+
+
     public static void main(String []argh)
     {
 
@@ -19,8 +22,14 @@ public class Solution{
             {
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127)System.out.println("* byte");
+                if(x>= Short.MIN_VALUE && x<=Short.MAX_VALUE)
+                    System.out.println("* byte");
+                if(x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE)
+                    System.out.println("* int");
+                if(x>=Long.MIN_VALUE && x<=Long.MAX_VALUE)
+                    System.out.println("* long");
                 //Complete the code
+
             }
             catch(Exception e)
             {
