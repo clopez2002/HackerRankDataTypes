0b1e42895eaffa5d8e9d4cc649dc7a4db8d3a5e5
lopezlin <morgan_pirata2003@yahoo.com.ar>
2021-06-22 21:45:14 -0300

    agregamos un if

--- src/Solution.java
+++ src/Solution.java
@@ -24,6 +24,8 @@ public class Solution{
                 System.out.println(x+" can be fitted in:");
                 if(x>= Short.MIN_VALUE && x<=Short.MAX_VALUE)
                     System.out.println("* byte");
+                if(x>=Short.MIN_VALUE && x<=Short.MAX_VALUE)
+                    System.out.println("* short");
                 if(x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE)
                     System.out.println("* int");
                 if(x>=Long.MIN_VALUE && x<=Long.MAX_VALUE)
